37e1a97529341eb14226f3ee6cd90a4c3086f758
Rajeev <rajgupta123@gmail.com>
2022-06-25 12:17:44 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitHubProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
